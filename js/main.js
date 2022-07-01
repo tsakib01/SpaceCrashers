b7e17c16130243384cccb5aa0f286fb76b325a8e
tsakib <tosrif_jahan_sakib@sfu.ca>
2022-07-01 13:38:07 -0700

    fixed display of text in hero images

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ const nextImageDelay = 5000;
 let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
 
 // slideshowImages[currentImageCounter].style.display = "block";
-slideshowImages[currentImageCounter].style.opacity = 1;
+slideshowImages[currentImageCounter].style.display = "block";
 
 setInterval(nextImage, nextImageDelay);
 
 function nextImage() {
-  // slideshowImages[currentImageCounter].style.display = "none";
-  slideshowImages[currentImageCounter].style.opacity = 0;
+  slideshowImages[currentImageCounter].style.display = "none";
+  // slideshowImages[currentImageCounter].style.opacity = 0;
 
   currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
 
-  // slideshowImages[currentImageCounter].style.display = "block";
-  slideshowImages[currentImageCounter].style.opacity = 1;
+  slideshowImages[currentImageCounter].style.display = "block";
+  // slideshowImages[currentImageCounter].style.opacity = 1;
 }
